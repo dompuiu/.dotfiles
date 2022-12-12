507d9a37a4599a658c3f9de3eb25b32c3c5067d2
Serban Stancu <stancu@adobe.com>
2022-12-11 20:26:07 -0700

    Add Telescope keybindings.

--- .config/nvim/after/plugin/keymaps.lua
+++ .config/nvim/after/plugin/keymaps.lua
@@ -1,6 +1,13 @@
-local keymap = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
-local expr_opts = { noremap = true, expr = true, silent = true }
+local keymap = vim.keymap.set
+local default_opts = {
+    noremap = true,
+    silent = true
+}
+local expr_opts = {
+    noremap = true,
+    expr = true,
+    silent = true
+}
 
 -- Open Netrw
 keymap("n", "<leader>pv", ":Ex<CR>", default_opts)
@@ -13,3 +20,10 @@ keymap("n", "n", "nzzzv", default_opts)
 keymap("n", "N", "Nzzzv", default_opts)
 keymap("n", "<C-d>", "<C-d>zz", default_opts)
 keymap("n", "<C-u>", "<C-u>zz", default_opts)
+
+-- Telescope
+local builtin = require("telescope.builtin")
+keymap("n", "<leader>ff", builtin.find_files, {})
+keymap("n", "<leader>fg", builtin.live_grep, {})
+keymap("n", "<leader>fb", builtin.buffers, {})
+keymap("n", "<leader>fh", builtin.help_tags, {})
