b0fa2065a93906f1c384a0898839a02c1915c089
Serban Stancu <stancu@adobe.com>
2022-12-13 15:51:04 -0700

    Format change.

--- .config/nvim/lua/dompuiu/packer.lua
+++ .config/nvim/lua/dompuiu/packer.lua
@@ -1,21 +1,20 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use("nvim-lua/plenary.nvim")
-  use("nvim-lua/popup.nvim")
-  use("nvim-telescope/telescope.nvim")
-  use("nvim-treesitter/nvim-treesitter", {
-    run = ":TSUpdate"
-  })
-  use ('nvim-telescope/telescope-fzf-native.nvim', {
-    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-  })
+    use("nvim-lua/plenary.nvim")
+    use("nvim-lua/popup.nvim")
+    use("nvim-telescope/telescope.nvim")
+    use("nvim-treesitter/nvim-treesitter", {
+        run = ":TSUpdate"
+    })
+    use('nvim-telescope/telescope-fzf-native.nvim', {
+        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    })
 
     use "sainnhe/sonokai"
 end)
