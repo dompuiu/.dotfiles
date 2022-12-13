c59137efdd4febab71497e619bd333340380af18
Serban Stancu <stancu@adobe.com>
2022-12-13 16:28:18 -0700

    Change formatting.

--- .config/nvim/after/plugin/autocmds.lua
+++ .config/nvim/after/plugin/autocmds.lua
@@ -4,27 +4,29 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 autocmd('TextYankPost', {
-  group = yank_group,
-  pattern = '*',
-  callback = function()
-      vim.highlight.on_yank({
-          higroup = 'IncSearch',
-          timeout = 40,
-      })
-  end,
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40
+        })
+    end
 })
 
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
 autocmd({"BufWritePre"}, {
-  group = ThePrimeagenGroup,
-  pattern = "*",
-  command = "%s/\\s\\+$//e",
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e"
 })
 
 -- Start Netrw when editor is launched
-local initNetrwGroup = augroup("InitNetrw", { clear = true })
+local initNetrwGroup = augroup("InitNetrw", {
+    clear = true
+})
 autocmd("VimEnter", {
-  pattern = "*",
-  command = [[if expand("%") == "" | silent! Explore | endif]],
-  group = initNetrwGroup,
+    pattern = "*",
+    command = [[if expand("%") == "" | silent! Explore | endif]],
+    group = initNetrwGroup
 })
