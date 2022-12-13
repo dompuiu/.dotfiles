d851faea5ed5b8e41eada1414dccfc4a0af46717
dompuiu <dompuiu@gmail.com>
2022-12-13 10:23:53 -0700

    Move some config variables to set.lua. Add some autocommands.

--- .config/nvim/after/plugin/autocmds.lua
+++ .config/nvim/after/plugin/autocmds.lua
@@ -1,8 +1,29 @@
-local api = vim.api
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+      vim.highlight.on_yank({
+          higroup = 'IncSearch',
+          timeout = 40,
+      })
+  end,
+})
+
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+autocmd({"BufWritePre"}, {
+  group = ThePrimeagenGroup,
+  pattern = "*",
+  command = "%s/\\s\\+$//e",
+})
 
 -- Start Netrw when editor is launched
-local initNetrwGroup = api.nvim_create_augroup("InitNetrw", { clear = true })
-api.nvim_create_autocmd("VimEnter", {
+local initNetrwGroup = augroup("InitNetrw", { clear = true })
+autocmd("VimEnter", {
   pattern = "*",
   command = [[if expand("%") == "" | silent! Explore | endif]],
   group = initNetrwGroup,
--- .config/nvim/lua/dompuiu/set.lua
+++ .config/nvim/lua/dompuiu/set.lua
@@ -45,3 +45,19 @@ vim.opt.shortmess:append("c")
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+-- Netrw Config
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.g.netrw_keepdir = 0
+
+vim.opt.list = true
+vim.opt.listchars = {
+  space = '·',
+  tab = '🡢\\',
+  nbsp = '·',
+  trail = '·',
+  extends = '>',
+  precedes = '<'
+}
