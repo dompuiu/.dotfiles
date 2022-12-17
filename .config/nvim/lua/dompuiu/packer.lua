62185083675fe28bba261c39b167e45f4aa749e0
dompuiu <dompuiu@gmail.com>
2022-12-17 09:33:23 -0700

    Change the way telescope is added.

--- .config/nvim/lua/dompuiu/packer.lua
+++ .config/nvim/lua/dompuiu/packer.lua
@@ -11,8 +11,10 @@ return require('packer').startup(function(use)
         run = ':TSUpdate'
     }
 
-    use("nvim-lua/plenary.nvim")
-    use("nvim-telescope/telescope.nvim")
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
 
     use('nvim-telescope/telescope-fzf-native.nvim', {
         run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
