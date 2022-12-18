c3d2923100ce308e6cdaaf846a8609b47f883841
dompuiu <dompuiu@gmail.com>
2022-12-17 21:43:07 -0700

    Use double quotes.

--- .config/nvim/after/plugin/indent_blankline.lua
+++ .config/nvim/after/plugin/indent_blankline.lua
@@ -4,11 +4,12 @@ if not status_ok then
 end
 
 indent_blankline.setup {
+    char = "┊",
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
     use_treesitter = true,
     use_treesitter_context = true,
-    context_highlight_list = {'rainbowcol1', 'rainbowcol2', 'rainbowcol3', 'rainbowcol4', 'rainbowcol5', 'rainbowcol6',
-                              'rainbowcol7'}
+    context_highlight_list = {"rainbowcol1", "rainbowcol2", "rainbowcol3", "rainbowcol4", "rainbowcol5", "rainbowcol6",
+                              "rainbowcol7"}
 }
