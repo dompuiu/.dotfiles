cf6d6076b641bfccbfe633a60b82c7f91a895c4a
dompuiu <dompuiu@gmail.com>
2022-12-26 09:52:20 -0700

    Add some comments.

--- .config/nvim/after/plugin/keymaps.lua
+++ .config/nvim/after/plugin/keymaps.lua
@@ -12,8 +12,20 @@ local expr_opts = {
 -- Cancel search highlighting with ESC
 keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
 
+-- Move selected lines
+keymap("v", "J", ":m '>+1<cr>gv=gv", default_opts)
+keymap("v", "K", ":m '<-2<cr>gv=gv", default_opts)
+
+-- Append the bottom line to the current line
+keymap("n", "J", "mzJ`z", default_opts)
+
 -- Center search results
 keymap("n", "n", "nzzzv", default_opts)
 keymap("n", "N", "Nzzzv", default_opts)
+
+-- Center the half page scroll
 keymap("n", "<C-d>", "<C-d>zz", default_opts)
 keymap("n", "<C-u>", "<C-u>zz", default_opts)
+
+-- Replace the word under cursor
+keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
