a79592b1924a065f65833d6f8eddca4d3f3f18dd
Serban Stancu <stancu@adobe.com>
2023-01-05 10:52:57 -0700

    Add targets plugin.

--- .config/nvim/lua/dompuiu/packer.lua
+++ .config/nvim/lua/dompuiu/packer.lua
@@ -102,5 +102,6 @@ return require("packer").startup(function(use)
     }
 
     use "simrat39/rust-tools.nvim"
-    use 'mfussenegger/nvim-dap'
+    use "mfussenegger/nvim-dap"
+    use "wellle/targets.vim"
 end)
