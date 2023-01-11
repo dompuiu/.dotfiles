9b7249020a0b69d09979912d9ab69599a9a330ed
Serban Stancu <stancu@adobe.com>
2023-01-10 18:36:24 -0700

    Remove unused var.

--- .config/nvim/after/plugin/keymaps.lua
+++ .config/nvim/after/plugin/keymaps.lua
@@ -3,11 +3,6 @@ local default_opts = {
     noremap = true,
     silent = true
 }
-local expr_opts = {
-    noremap = true,
-    expr = true,
-    silent = true
-}
 
 -- Cancel search highlighting with ESC
 keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
