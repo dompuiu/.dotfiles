818c7c9696f4ecffd3a6a1fb1ddcc87ea07b81ae
dompuiu <dompuiu@gmail.com>
2023-01-30 00:18:11 -0700

    Add some plugins.

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+          require("telescope").load_extension("file_browser")
+        end,
+      },
+    },
+  },
+}
