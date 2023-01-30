c22747e8f284873439fcc10601ac7340f2f674fe
dompuiu <dompuiu@gmail.com>
2023-01-30 00:58:57 -0700

    Add new find command that ignores .gitignore.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,13 @@ return {
   -- add telescope-fzf-native
   {
     "telescope.nvim",
+    keys = {
+      {
+        "<leader>fa",
+        "<cmd>Telescope find_files find_command=fd,-I<cr>",
+        desc = "Find in all files (ignore .gitignore)",
+      },
+    },
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
