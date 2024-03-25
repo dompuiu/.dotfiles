87d90285edda78e6d7e875f1e522b492c8bf88fa
Serban Stancu <stancu@adobe.com>
2024-03-25 14:39:59 -0600

    Try various color schemes.

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "storm",
+      -- on_colors = function(colors)
+      --   print(vim.inspect(colors))
+      -- end,
+      -- on_highlights = function(highlights, colors)
+      --   print(vim.inspect(highlights))
+      -- end,
+    },
+  },
+
+  -- add kanagawa
+  -- { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "kanagawa",
+  --   },
+  -- },
+}
