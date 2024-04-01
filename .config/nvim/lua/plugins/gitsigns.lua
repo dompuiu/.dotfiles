17948a5e9a20f68a9e98f63897d4730b8fb7258b
dompuiu <dompuiu@gmail.com>
2024-03-31 22:03:38 -0600

    Add option to toggle git line blame.

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "gitsigns.nvim",
+    keys = {
+      {
+        "<leader>gb",
+        "<cmd>Gitsigns toggle_current_line_blame<cr>",
+        desc = "Git Toggle Current Line Blame",
+      },
+    },
+  },
+}
