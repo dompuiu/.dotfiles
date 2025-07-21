ece3971d73c9b9c22f595d6bfd661c00a30ab624
Serban Stancu <stancu@adobe.com>
2025-07-21 16:43:51 -0600

    Change avante model.

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -19,7 +19,7 @@ return {
         provider = "copilot",
         providers = {
           copilot = {
-            model = "claude-3.7-sonnet",
+            model = "claude-sonnet-4",
           },
         },
         web_search_engine = {
