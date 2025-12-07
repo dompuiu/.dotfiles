84d51dd23e430325459da3fc4a6a2502678749a8
Serban Stancu <dompuiu@gmail.com>
2025-12-07 11:51:15 -0700

    Don't load avante on Ubuntu.

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,25 +1,31 @@
-return {
-  {
-    "yetone/avante.nvim",
-    opts = {
-      provider = "copilot",
-      providers = {
-        copilot = {
-          model = "claude-sonnet-4.5",
+local is_ubuntu = vim.fn.has("wsl") == 1 or vim.loop.os_uname().version:match("Ubuntu")
+
+if not is_ubuntu then
+  return {
+    {
+      "yetone/avante.nvim",
+      opts = {
+        provider = "copilot",
+        providers = {
+          copilot = {
+            model = "claude-sonnet-4.5",
+          },
+        },
+        selector = {
+          provider = "fzf_lua",
+        },
+        selection = {
+          hint_display = "none",
+        },
+        behaviour = {
+          auto_set_keymaps = false,
+        },
+        web_search_engine = {
+          provider = "brave",
         },
       },
-      selector = {
-        provider = "fzf_lua",
-      },
-      selection = {
-        hint_display = "none",
-      },
-      behaviour = {
-        auto_set_keymaps = false,
-      },
-      web_search_engine = {
-        provider = "brave",
-      },
     },
-  },
-}
+  }
+else
+  return {}
+end
